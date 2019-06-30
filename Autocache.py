651fa2ac9df22e9e5c03f34364ec51fd11120a14
root <root@localhost.localdomain>
2019-06-30 10:47:21 +0000

    Add console call to sim cache

--- Autocache.py
+++ Autocache.py
@@ -1,5 +1,10 @@
 import os
-Benchmarks = []
+Benchmarks = [
+    "Benchmarks/amp.ss", 
+    "Benchmarks/basicmath.ss",
+    "Benchmarks/cc1.ss -O Benchmarks/1stmt.i",
+    "Benchmarks/mm.ss"
+    ]
 class Cache:
     cacheSize = 4096
     blockSize = 32
@@ -12,11 +17,20 @@ class Cache:
         self.cacheSize = cacheSize
         self.blockSize = blockSize
         self.associativity = associativity
+    def arrangements(self):
+        return self.cacheSize/(self.blockSize * self.associativity)
     def printCache(self):
         print("CacheSize: %d\nBlockSize: %d\nAssociativity: %d\nHits: %d \nMisses: %d \nLatency: %f \nEnergy: %f" % (self.cacheSize, self.blockSize, self.associativity, self.hits, self.misses, self.cacheLatency, self.cacheEnergy))
     def runSimCache(self, path):
-        os.system("./%s" % path)
-
+        os.system("./%s -cache:il1 il1:%d:%d:%d:l -cache:il2 none -cache:dl1 dl1:%d:%d:%d:l -cache:dl2 none -redir:sim temp.txt %s" % (
+            path, 
+            self.arrangements(), 
+            self.blockSize, 
+            self.associativity,
+            self.arrangements(),
+            self.blockSize,
+            self.associativity,
+            Benchmarks[0]))
 
 cache = Cache(4096, 32, 8)
 cache.runSimCache("simplesim-3.0/sim-cache")
